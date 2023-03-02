f044e635d231300cd2aa46aaae467fd6b1b0195d
Orlando <galunggong14@gmail.com>
2023-03-01 20:25:10 -0800

    karaoke project

--- karaoke/server/config/mongoose.config.js
+++ karaoke/server/config/mongoose.config.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const db_name = "karaoke";
 
-mongoose.connect("mongodb://localhost/" + db_name, {
+mongoose.connect("mongodb://127.0.0.1:27017/" + db_name, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => console.log("successfully connected to the " + db_name + "database"))
+    .then(() => console.log("successfully connected to the " + db_name + " database"))
     .catch((err) => {
         console.log("cannot connect to database" + db_name + ":")
         console.log(err);
--- karaoke/server/models/karaoke.model.js
+++ karaoke/server/models/karaoke.model.js
@@ -6,7 +6,34 @@ const karaokeSchema = new mongoose.Schema({
         required: [true, "You must have title for karaoke song"],
         minlength: [3, "Your soong mustbe be 3 characters long"],
     },
-    
+    artist: {
+        type: String,
+        minlength: [10,"Album art URL must be at Least 5 chars long"],
+    },
+    albumArtUrl: {
+        type: String,
+        minlength: [10, "Your album art should be 10 char long"],
+    },
+    videoURL: {
+        type: String,
+        required: [true, " must have a little for a weekend karayoke"],
+    },
+    gender:{
+        type: String,
+        required: [true, " must have10 char long"],
+        enum: ["Pop", "Country", "Hiphop"],
+        minlength: [3, "Your song must be at least 3 chars long"],
+    },
+    year: {
+        type: Number,
+        required: [true, "The song must be wrtten no earlier than 1930"],
+    },
+    lisenced: {
+        type: Boolean,
+        default: true,
+    },
+
+
 },{ timestamps: true});
 
 module.exports = mongoose.model("Karaoke", karaokeSchema);
