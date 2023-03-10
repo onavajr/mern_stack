5c4e15cefd9f4e6dae6abba395c8740a741f6bc8
Orlando <galunggong14@gmail.com>
2023-03-09 23:48:40 -0800

    list alll

--- karaoke/client/src/components/ListAll.js
+++ karaoke/client/src/components/ListAll.js
@@ -3,9 +3,36 @@ import {Link, Navigate} from '@reach/router';
 import axios from 'axios';
 
 const ListAll = (props) => {
+    const [allSongs, setAllSongs] = useState([]);
+    useEffect(()=> {
+        axios.get("http://localhost:8000/api/karaoke")
+        .then((res) => {
+            console.log(res.data);
+            setAllSongs(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        ;
+    },[]);
+
     return(
         <div>
-            <h1>List All</h1>
+            <table>
+                <thead>
+                    <th>Song tiltle</th>
+                    <th>Action Available</th>
+                    <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </thead>
+            </table>
         </div>
     )
 }
