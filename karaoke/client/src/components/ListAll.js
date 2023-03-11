24c67a72b7ba261a65d0427d851ff553a363693d
Orlando <galunggong14@gmail.com>
2023-03-10 21:03:56 -0800

    view page

--- karaoke/client/src/components/ListAll.js
+++ karaoke/client/src/components/ListAll.js
@@ -38,3 +38,44 @@ const ListAll = (props) => {
 }
 
 export default ListAll;
+
+import React, {useEffect, useState} from 'react';
+import {Link, Navigate} from '@reach/router';
+import axios from 'axios';
+
+const ListAll = (props) => {
+    const [allSongs, setAllSongs] = useState([]);
+    useEffect(()=> {
+        axios.get("http://localhost:8000/api/karaoke")
+        .then((res) => {
+            console.log(res.data);
+            setAllSongs(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        ;
+    },[]);
+
+    return(
+        <div>
+            <table>
+                <thead>
+                    <th>Song tiltle</th>
+                    <th>Action Available</th>
+                    <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </thead>
+            </table>
+        </div>
+    )
+}
+
+export default ListAll;
