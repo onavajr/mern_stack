f2d7cc0253aa17b2f1e8f237f980afac4aa8ce74
Orlando <galunggong14@gmail.com>
2023-03-26 21:44:37 -0700

    candy controller

--- candyshop/server/controller/candy.controller.js
+++ candyshop/server/controller/candy.controller.js
@@ -1,4 +1,4 @@
-const Candy = require('../nideks/candy.models');
+const Candy = require('../models/candy.models');
 
 module.exports({
     findAll: (req, res) => {
@@ -8,12 +8,15 @@ module.exports({
     },
 
     findOne: (req, res) => {
-        Candy.findById(req,params.id)
+        Candy.findById(req, params.id)
             .then(candy => res.json(candy))
-            .catch(err => res.json({message:"Something went wrong", error:err}));
+            .catch(err => res.json({message:"Something went wrong", error:err}))
     },
 
     create: (req,res) => {
+        Candy.create(req.body)
+        .then((newCandy) => res.json(newCandy))
+        .catch(err => res.json({ message: "Something went wrong", error:err}))
         
     }
-})
+});
