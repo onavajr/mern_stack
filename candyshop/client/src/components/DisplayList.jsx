f95fb8c059f413ca2c588db33ba49f441ea80d1c
Orlando <galunggong14@gmail.com>
2023-04-03 22:44:12 -0700

    Nav bar path

---
+++ candyshop/client/src/components/DisplayList.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const DisplayList = (props) => {
+    return (
+        <div>
+            Display List Components
+        </div>
+    )
+};
+
+export default DisplayList;  
