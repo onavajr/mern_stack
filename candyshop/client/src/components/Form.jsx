64d082f7550ae35e69cc79b49747d12826262d46
Orlando <galunggong14@gmail.com>
2023-04-09 21:24:55 -0700

    models

--- candyshop/client/src/components/Form.jsx
+++ candyshop/client/src/components/Form.jsx
@@ -1,30 +1,30 @@
 
-// import {useState} from 'react';
+import {useState} from 'react';
 
-// const Form = (props) => {
-//     const [name, setName] = useState("");
-//     const [price, setPrice] = useState(0);
-//     const [description, setDescription] = useState("");
-//     return (
-//         <div>
-//             <h1>Form Components</h1>
+const Form = (props) => {
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState("");
+    return (
+        <div>
+            <h1>Form Components</h1>
 
-//             <form>
-//                 <div>
-//                     <label>Name</label>
-//                     <input onChangetype = {(e) => setName(e.target.value)} type='text'/>
-//                 </div>
-//                 <div>
-//                     <label>Name</label>
-//                     <input type='text'/>
-//                 </div>
-//                 <div>
-//                     <label>Name</label>
-//                     <input type='text'/>
-//                 </div>
-//             </form>
-//         </div>
-//     );
-// };
+            <form>
+                <div>
+                    <label>Name</label>
+                    <input onChangetype = {(e) => setName(e.target.value)} type='text'/>
+                </div>
+                <div>
+                    <label>Name</label>
+                    <input type='text'/>
+                </div>
+                <div>
+                    <label>Name</label>
+                    <input type='text'/>
+                </div>
+            </form>
+        </div>
+    );
+};
 
-// export default Form;
+export default Form;
