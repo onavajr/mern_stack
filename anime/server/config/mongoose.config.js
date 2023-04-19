fc1c7ff2ccb51a3013bde20a35284799a5f2cc40
Orlando <galunggong14@gmail.com>
2023-04-18 21:30:32 -0700

    anime mongoose config

---
+++ anime/server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/anime', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    .then(() => console.log('Connected to the database'))
+    .catch((err) => console.log('Failed to connet to the database', err));
