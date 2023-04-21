cd5410300e0f1b9acf0b1ca5d2fa379b7041e7a3
Orlando <galunggong14@gmail.com>
2023-04-20 22:13:48 -0700

    anime controller

--- anime/server/controller/anime.controller.js
+++ anime/server/controller/anime.controller.js
@@ -1,1 +1,9 @@
-const Anime = require('models/');
+const Anime = require('../models/anime.models');
+
+module.exports = {
+    findAll: (req, res) => {
+        Candy.find()
+        .then((allAnime) => res.json(allAnime));
+    },
+}
+
