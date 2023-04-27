233f222448417eb559968e7fd7d6d84ca9ecb3c2
Orlando <galunggong14@gmail.com>
2023-04-26 21:59:03 -0700

    anime controller

--- anime/server/controller/anime.controller.js
+++ anime/server/controller/anime.controller.js
@@ -3,7 +3,20 @@ const Anime = require('../models/anime.models');
 module.exports = {
     findAll: (req, res) => {
         Anime.find()
-        .then((allAnime) => res.json(allAnime));
+            .then((allCandies) => res.json(allCandies))
+            .catch((err) => res.json({message: "Something went wrong", error:  err}));
     },
-}
 
+    findOne: (req, res) => {
+        Anime.findById(req, params.id)
+            .then((anime) => res.json(anime))
+            .catch((err) => res.json({message:"Something went wrong", error:err}));
+    },
+
+    create: (req,res) => {
+        Anime.create(req.body)
+        .then((newAnime) => res.json(newAnime))
+        .catch((err)=> res.json({ message: "Something went wrong", error:err}));
+        
+    }
+};
