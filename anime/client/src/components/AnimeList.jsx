5c5f6594de3952b50ff0ee362e9f34958de14997
Orlando <galunggong14@gmail.com>
2023-04-30 21:33:59 -0700

    Anime List Front Page

---
+++ anime/client/src/components/AnimeList.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const DisplayList = (props) => {
+    return (
+        <div>
+            <h1>Anime List Components</h1>
+        </div>
+    )
+};
+
+export default DisplayList;  
