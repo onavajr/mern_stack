04cfc1dd8c405f699bc01682d7daf7fe9ca2e5d3
orlando <galunggong14@gmail.com>
2023-10-09 20:37:59 -0700

    .env

---
+++ newproject/backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/',(req, res) => {
+    res.json({msg: 'Welcome to my webpage'})
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening to port', process.env.PORT)
+})
+
+
