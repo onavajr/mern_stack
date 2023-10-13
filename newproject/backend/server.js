c365a723f13db05d4a5efe670e7f24b17c5b6bc2
Orlando Nava <galunggong14@gmail.com>
2023-10-12 22:46:22 -0700

    back end setup

--- newproject/backend/server.js
+++ newproject/backend/server.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const myroutes = require('./routes/routes')
 const app = express()
+
+app.use((req, res, next) => {
+    console.log(rq.path, req.method)
+})
 
 app.get('/',(req, res) => {
     res.json({msg: 'Welcome to my webpage'})
