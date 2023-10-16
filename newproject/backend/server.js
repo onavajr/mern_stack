fbcd2d692ea222d6da6b2d88b8b17972bd908594
Orlando Nava <galunggong14@gmail.com>
2023-10-15 23:19:48 -0700

    middle ware with next fucntion

--- newproject/backend/server.js
+++ newproject/backend/server.js
@@ -1,16 +1,21 @@
 require('dotenv').config()
 const express = require('express')
-const myroutes = require('./routes/routes')
+
+//exrpess app
 const app = express()
 
+//middleware
 app.use((req, res, next) => {
     console.log(req.path, req.method)
+    next()
 })
 
+//routes
 app.get('/',(req, res) => {
     res.json({msg: 'Welcome to my webpage'})
 })
 
+//listen for request
 app.listen(process.env.PORT, () => {
     console.log('Listening to port', process.env.PORT)
 })
