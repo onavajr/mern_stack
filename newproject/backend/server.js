311da5f96a9c4b4f1350875547070bd302bee830
Orlando Nava <galunggong14@gmail.com>
2023-10-24 04:57:23 -0700

    mongoose connect

--- newproject/backend/server.js
+++ newproject/backend/server.js
@@ -18,9 +18,17 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/newproject', newprojectRoutes)
 
-//listen for request
-app.listen(process.env.PORT, () => {
-    console.log('Listening to port', process.env.PORT)
-})
+//connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for request
+        app.listen(process.env.PORT, () => {
+            console.log('Listening to port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 
 
